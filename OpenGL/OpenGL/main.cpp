026b2837c4a5a5dda2dce6ffe43a7cfb6fb480b3
Bartosz Wajsowicz <b.wajsowicz@gmail.com>
2017-10-07 14:42:32 +0200

    Tesselation

--- OpenGL/OpenGL/main.cpp
+++ OpenGL/OpenGL/main.cpp
@@ -7,19 +7,13 @@ public:
 	{
 		GLuint vertexShader;
 		GLuint fragmentShader;
+		GLuint tcsShader;
+		GLuint tesShader;
 		GLuint program;
 
 		static const GLchar *vShaderSource[] =
 		{
-			"#version 330 core                                                 \n"
-			"                                                                  \n"
-			"layout (location = 0) in vec4 offset;                             \n"
-			"layout (location = 1) in vec4 color;                              \n"
-			"                                                                  \n"
-			"out VS_OUT                                                        \n"
-		    "{																   \n"
-			"	vec4 color;													   \n"
-			"}vs_out;                                                          \n"
+			"#version 410 core                                                 \n"
 			"                                                                  \n"
 			"void main(void)                                                   \n"
 			"{                                                                 \n"
@@ -27,26 +21,51 @@ public:
 			"                                   vec4(-0.25, -0.25, 0.5, 1.0),  \n"
 			"                                   vec4( 0.25,  0.25, 0.5, 1.0)); \n"
 			"                                                                  \n"
-			"    gl_Position = vertices[gl_VertexID] + offset;                 \n"
-			"    vs_out.color = color;										   \n"
+			"    gl_Position = vertices[gl_VertexID];                  \n"
 			"}																   \n"
 		};
 
 		static const GLchar *fShaderSource[] =
 		{
-			"#version 330 core                          \n"
-			"                                           \n"
-			"in VS_OUT                                  \n"
-			"{                                          \n"
-			"	vec4 color;                             \n"
-			"}fs_in;                                    \n"
+			"#version 410 core                          \n"
 			"                                           \n"
 			"out vec4 color;                            \n"
 			"                                           \n"
 			"void main(void)                            \n"
 			"{                                          \n"
-			"	color = fs_in.color;                    \n"
+			"	color = vec4(0.0, 0.8, 1.0, 1.0);                    \n"
 			"}                                          \n"
+		};
+		static const GLchar *tcsShaderSource[] =
+		{
+			"#version 410 core                                                                 \n"
+			"                                                                                  \n"
+			"layout (vertices = 3) out;                                                        \n"
+			"                                                                                  \n"
+			"void main(void)                                                                   \n"
+			"{                                                                                 \n"
+			"    if (gl_InvocationID == 0)                                                     \n"
+			"    {                                                                             \n"
+			"        gl_TessLevelInner[0] = 5.0;                                               \n"
+			"        gl_TessLevelOuter[0] = 5.0;                                               \n"
+			"        gl_TessLevelOuter[1] = 5.0;                                               \n"
+			"        gl_TessLevelOuter[2] = 5.0;                                               \n"
+			"    }                                                                             \n"
+			"    gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;     \n"
+			"}														\n"
+		};
+
+		static const GLchar *tesShaderSource[] = {
+			"#version 410 core                                                                 \n"
+			"                                                                                  \n"
+			"layout (triangles, equal_spacing, cw) in;                                         \n"
+			"                                                                                  \n"
+			"void main(void)                                                                   \n"
+			"{                                                                                 \n"
+			"    gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position) +                       \n"
+			"                  (gl_TessCoord.y * gl_in[1].gl_Position) +                       \n"
+			"                  (gl_TessCoord.z * gl_in[2].gl_Position);                        \n"
+			"}                                                          \n"
 		};
 
 		vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -56,9 +75,19 @@ public:
 		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 		glShaderSource(fragmentShader, 1, fShaderSource, NULL);
 		glCompileShader(fragmentShader);
+		
+		tcsShader = glCreateShader(GL_TESS_CONTROL_SHADER);
+		glShaderSource(tcsShader, 1, tcsShaderSource, NULL);
+		glCompileShader(tcsShader);
+
+		tesShader = glCreateShader(GL_TESS_EVALUATION_SHADER);
+		glShaderSource(tesShader, 1, tesShaderSource, NULL);
+		glCompileShader(tesShader);
 
 		program = glCreateProgram();
 		glAttachShader(program, vertexShader);
+		glAttachShader(program, tcsShader);
+		glAttachShader(program, tesShader);
 		glAttachShader(program, fragmentShader);
 		glLinkProgram(program);
 
@@ -71,28 +100,19 @@ public:
 	void startup()
 	{
 		renderingProgram = compile_shaders();
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glGenVertexArrays(1, &VAO);
 		glBindVertexArray(VAO);
 	}
 
 	void render(double currentTime)
 	{
-		static const GLfloat red[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-		glClearBufferfv(GL_COLOR, 0, red);
+		static const GLfloat green[] = { 0.0f, 0.25f, 0.0f, 1.0f };
+		glClearBufferfv(GL_COLOR, 0, green);
 
 		glUseProgram(renderingProgram);
 
-		GLfloat attrib[] = { (float)sin(currentTime)*0.5f,
-							(float)cos(currentTime)*0.6f,
-							0.0f, 0.0f };
-		glVertexAttrib4fv(0, attrib);
-
-		GLfloat attrib_01[] = { (float)sin(currentTime)*0.5f,
-			(float)cos(currentTime)*0.6f,
-			0.0f, 0.0f };
-		glVertexAttrib4fv(1, attrib);
-
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_PATCHES, 0, 3);
 	}
 
 	void shutdown()
